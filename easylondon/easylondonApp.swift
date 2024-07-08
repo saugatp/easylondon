35f4aab35b3fe5a0dadcd5dc396f82eecd24c449
Saugat Poudel <poudelsaugat45@gmail.com>
2024-07-08 21:07:11 +0100

    Initial Commit

---
+++ easylondon/easylondonApp.swift
@@ -0,0 +1,17 @@
+//
+//  easylondonApp.swift
+//  easylondon
+//
+//  Created by Saugat Poudel on 08/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct easylondonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
